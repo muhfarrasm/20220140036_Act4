a76ac1b8a4293825f6e8a30ed600ecfa58cad522
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-23 19:26:41 +0700

    Mendeklarasikan variable

---
+++ lib/presentation/order_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahmakananController = TextEditingController();
+  final TextEditingController jumlahminumanController = TextEditingController();
+  int totalHarga = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
