f3b8833e198e011602ba3c080d3fc7dd33a6cdd1
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-23 17:46:39 +0700

    Add import registerpage

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/register_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
