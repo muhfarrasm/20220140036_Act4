17371f48a0a733013cb9abd8794e612ae5180ca8
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-23 20:09:08 +0700

    Create parameter konstruktor

---
+++ lib/presentation/order_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/home_page.dart';
+
+class OrderDetailPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+  const OrderDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
