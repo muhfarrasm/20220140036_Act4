4a494670619c34841ecebf73c07dbf28dca78055
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-23 18:02:09 +0700

    Create controller email dan password fix

---
+++ lib/presentation/register_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
