a6190d9b2305612d2dbb371499ea69024ae37abe
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-23 20:12:31 +0700

    parameter dengan required

--- lib/presentation/order_detail_page.dart
+++ lib/presentation/order_detail_page.dart
@@ -7,7 +7,15 @@ class OrderDetailPage extends StatelessWidget {
   final String jumlahMakanan;
   final String jumlahMinuman;
   final int totalHarga;
-  const OrderDetailPage({super.key});
+  const OrderDetailPage({
+    super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+    
+  });
 
   @override
   Widget build(BuildContext context) {
