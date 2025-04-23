c7d6e39b387bd42e7548999a0eced2edcd33506a
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-23 20:22:17 +0700

    Create ElevatedButton

--- lib/presentation/order_detail_page.dart
+++ lib/presentation/order_detail_page.dart
@@ -32,11 +32,22 @@ class OrderDetailPage extends StatelessWidget {
             Text('Drink QTY Order: $jumlahMinuman'),
             Text('Total Price: $totalHarga'),
 
-            
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false,
+                
+                );
+              },
+              child: Text('Finish Order'),
+            )
 
           ],
-        )
-      )
-    )
+        ),
+      ),
+    );
   }
 }
