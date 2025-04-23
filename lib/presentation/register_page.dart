4e6d7e8abc43a9a2b5d257ed197d65be79298599
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-23 18:06:53 +0700

    Create body form register

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -15,9 +15,17 @@ class _RegisterPageState extends State<RegisterPage> {
   void initState(){
     super.initState();
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Form(child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
+        ),
+      )
+      )
+    )
   }
 }
